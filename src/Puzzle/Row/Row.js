f7fccf20f6dc8273819e51b22374da44870c5518
Bill McCutchan <bmccutchanjr@gmail.com>
2019-02-21 18:22:55 -0500

    Added some somments to <Row>

--- src/Puzzle/Row/Row.js
+++ src/Puzzle/Row/Row.js
@@ -1,3 +1,8 @@
+// <Row> is a simple component that displays 3 cells in a row inside the <Region> component.  I need
+// it to use a flex box to arrange and size the cells.  Now that it's written I'm thinking I might need
+// it to arrange and display <Region> inside the <Puzzle> and <CellNotes> inside a <Cell>.  <Row> is
+// pretty simple and already generic, so I should be able to do that with little problem.
+
 import React from "react";
 
 import "./Row.css";
